5b8627d3ee45c6a0e99cafec8081e6c18b66bb18
Vale189 <Valerja.chalaja58@gmail.com>
2024-01-29 12:24:43 +0200

    fixing

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+let check = prompt("Як тебе звати?", "Не знаю");
+switch (check) {
+	case "Саша": {
+		alert("Ти мій зайчик");
+	}
+	case "Лєра": {
+		alert("Я знаю");
+	}
+	case "Вася": {
+		alert("Ти хто такий");
+	}
+	default: {
+		alert("я тебе не знаю");
+	}
+}
