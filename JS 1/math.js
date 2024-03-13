edba829225f54e367ce5bd9037975aca39287335
Vale189 <Valerja.chalaja58@gmail.com>
2024-03-13 16:47:29 +0200

    JS 1, 2, 3

---
+++ JS 1/math.js
@@ -0,0 +1,33 @@
+// HW 1
+
+let userAgeValue = 18;
+let userNameNickname = "Sasha";
+
+userAgeValue = 25;
+userNameNickname = "Anya";
+
+// Math operations
+
+let firstValue = 8;
+let secondValue = 5;
+
+console.log(`5+8 = ${firstValue + secondValue}`);
+console.log(`8-5 = ${firstValue - secondValue}`);
+console.log(`5*8 = ${firstValue * secondValue}`);
+console.log(`8/5 = ${firstValue / secondValue}`);
+console.log(`8%5 = ${firstValue % secondValue}`);
+console.log(`8**5 = ${firstValue ** secondValue}`);
+
+//
+
+let result1 = (5 + 3) * 2 - 7;
+
+let result2 = (10 / 2) ** 2;
+
+let result3 = 2 * (4 + 3) - 8 / 2;
+
+let result4 = (15 % 4) + 5;
+
+let result5 = 3 - 2 ** 4;
+
+console.log(result1, result2, result3, result4, result5);
